6648d4b24c610df3ccdf21d3da99466b0bb9a92c
anm8tr <threeduser@hotmail.com>
2017-12-09 21:47:15 -0700

    commit 7   add user

---
+++ app/src/main/java/com/azmobiledev/fieldagentchat/CreateUserActivity.kt
@@ -0,0 +1,26 @@
+package com.azmobiledev.fieldagentchat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun generateColorClicked(view: View) {
+
+    }
+
+    fun createUserClicked(view: View) {
+
+
+    }
+}
---
+++ app/src/main/java/com/azmobiledev/fieldagentchat/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.azmobiledev.fieldagentchat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginBtnClicked(view: View) {
+
+    }
+
+    fun loginCreateUserBtnClicked(view: View) {
+
+
+    }
+}
