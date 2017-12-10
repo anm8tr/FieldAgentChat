2b20de7d17ba099b1253e01d786a19664c85b13d
anm8tr <threeduser@hotmail.com>
2017-12-09 21:57:35 -0700

    commit 8   add user intent

--- app/src/main/java/com/azmobiledev/fieldagentchat/LoginActivity.kt
+++ app/src/main/java/com/azmobiledev/fieldagentchat/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.azmobiledev.fieldagentchat
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,7 +17,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateUserBtnClicked(view: View) {
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
 
     }
 }
